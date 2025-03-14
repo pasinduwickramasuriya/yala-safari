981f71476bd2057f50dd1de1636042ed002d2a65
Pasindu sadanjana <131373684+pasinduwickramasuriya@users.noreply.github.com>
2025-03-14 10:38:46 +0530

    second

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '', // Leave empty for default (443 for HTTPS)
+        pathname: '/**', // Allow all paths under this hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;
